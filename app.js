21bbafb24b688600cfe59ac4c36a7ee269236320
andres <andres_l159@hotmail.com>
2025-12-16 20:55:39 -0500

    feat: Context middleware

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var middleware = require('./middleware');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var middleware = require("./middleware");
 
-var indexRouter = require('./routes/index');
-var recipesRouter = require('./routes/recipes');
+var indexRouter = require("./routes/index");
+var recipesRouter = require("./routes/recipes");
 
 var app = express();
 
 // view engine setup
-app.set('view engine', 'jade');
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
-app.use('/recipes', recipesRouter);
-
+app.use("/recipes", middleware, recipesRouter);
 
 module.exports = app;
--- middleware.js
+++ middleware.js
@@ -1,3 +1,10 @@
-module.exports = (req, res, next) => {
-
+module.exports = (req, _, next) => {
+  req.context = {
+    page: req.query.page || 1,
+    limit: req.query.limit || 3,
+    skip: ((req.query.page || 1) - 1) * (req.query.limit || 3),
+    searchTerm: req.query.search || "",
+    search: new RegExp(req.query.search || "", "gi"),
+  };
+  next();
 };
